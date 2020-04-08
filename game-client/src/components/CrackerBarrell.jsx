433c8fe1bef29ab8ac55b2227108072aea9e2b10
Josh Howe <josh.w.howe@gmail.com>
2020-04-08 18:43:52 -0400

    moved all logic from "render" to "handleClick". Still some issues with the "undo" function.

--- game-client/src/components/CrackerBarrell.jsx
+++ game-client/src/components/CrackerBarrell.jsx
@@ -10,35 +10,76 @@ export default class CrackerBarrell extends React.Component {
 	constructor(props) {
 		super(props);
 		
-		this.unslectPeg = this.unselectPeg.bind(this);
+		this.unselectPeg = this.unselectPeg.bind(this);
+		
+		let pegLocations = Array(15).fill(1).map((isEmpty,idx) => {
+			let retVal = this.props.emptyPeg === idx ? 0 : 1;
+			
+			return retVal;
+		});
+		
+		let selectablePegs = this.getSelectablePegs(pegLocations);
+		
 		
 		this.state = {
 			history: [{
-				pegLocations: Array(15).fill(1).map((isEmpty,idx) => {
-					let retVal = this.props.emptyPeg === idx ? 0 : 1;
-					
-					return retVal;
-				}),
+				selectedPeg: null,
+				pegLocations: pegLocations,
+				selectablePegs: selectablePegs, 
+				selectableHoles: null,
 				lastMove: null,
 			}],			
 			
-			selectedPeg:null,
 			gameOver:0,
 			pegsRemaining:null,
 			oldGames:null,
 		};
 	}
+	
+	getSelectablePegs(pegLocations) {
+		return pegLocations.map((hasPeg,idx) => {
+	   	if (hasPeg === 0) return 0;
+	   
+	   	const moves = this.props.validMoves[idx];
+	   	let retVal = 0;
+	   
+	   	for (let move of moves) {
+	    	if (pegLocations[move[0]] !== 0 && pegLocations[move[1]] !== 1)
+	      retVal = 1;     
+	   	}
+	   
+	   	return retVal;
+  	});		
+	}
+	
+	getSelectableHoles(pegLocations, selectedPeg) {
+  	return pegLocations.map((hasPeg,idx) => {
+	     if (hasPeg === 1) return 0;
+	     
+	     const moves = this.props.validMoves[selectedPeg];
+	     let retVal = 0;
+	     
+	     for (let move of moves) {
+	      if (move[1] === idx)
+	        retVal = 1;     
+	     }
+	     
+	     return retVal;
+  	});		
+	}	
 
 	unselectPeg(e) {
-		const history = this.state.history;	
+		document.removeEventListener('click', this.unselectPeg, false);
 		
-		document.removeEventListener('click', this.unslectPeg, false);
+		const tmpHistory = this.state.history.slice(0,this.state.history.length - 1);
 		
-  	this.setState ({
-			selectedPeg: null,
-				history: history,					 
-		});		
-	};
+		if (tmpHistory.length > 0) {		
+					
+			this.setState ({
+				history: tmpHistory,						
+			});
+		}
+	}
 	
   resetGame() {
 		let emptyPeg = Math.floor(Math.random() * Math.floor(14));
@@ -63,116 +104,103 @@ export default class CrackerBarrell extends React.Component {
 		let tmpHistory = this.state.history.slice(0,this.state.history.length - 1);
 		
 		if (tmpHistory.length > 0 && this.state.gameOver === 0) {		
-			//console.log(this.state.history.length);
-			//console.log(tmpHistory);
-					
 			this.setState ({
 				history: tmpHistory,						
-				selectedPeg:null,
-				gameOver:0,
 			});
 		}
 	}
 	
-	handleClick(i, selectablePegs, selectableHoles) {
-		const selectedPeg = this.state.selectedPeg;
+	handleClick(i) {
 		const history = this.state.history;
+		const current = history[history.length - 1];
+		console.log(history.length - 1);
 		
-		let pegLocations = history[history.length - 1].pegLocations.slice(0);
-		const validMoves = this.props.validMoves;
+		let gameOver = this.state.gameOver;
+		let pegsRemaining = this.state.pegsRemaining;
 		
-		if (selectedPeg === null) {		
-			if (selectablePegs[i]) { // only do anyting if the peg has a valid move
-				document.addEventListener('click', this.unslectPeg, false);	
+		console.log("Game Over: " + gameOver);
+		
+		if (gameOver !== 1) {
+		
+			const selectedPeg = current.selectedPeg;
+			const pegLocations = current.pegLocations;
+			const selectablePegs = current.selectablePegs;
+			const selectableHoles = current.selectableHoles;
+			const validMoves = this.props.validMoves;		
+						
+			if (selectedPeg === null) {	//user selected a peg	
+				if (selectablePegs[i]) { // only do anyting if the peg has a valid move
+					document.addEventListener('click', this.unselectPeg, false);	
 
-				this.setState ({
+					const selectableHoles = this.getSelectableHoles(pegLocations, i);
+
+			  	history.push({
 					selectedPeg: i,
-				});
+					pegLocations: pegLocations,
+					selectablePegs: selectablePegs, 
+					selectableHoles: selectableHoles,
+					lastMove: null,
+			  	});
+				}
+			} else {
+				if (selectableHoles[i]) {
+			  	let lastMove = validMoves[selectedPeg].filter((move) => {
+			  	  return move[1] === i;
+			  	});
+			  	
+			    pegLocations[lastMove[0][0]] = 0;
+			    pegLocations[lastMove[0][1]] = 1;
+			    pegLocations[selectedPeg] = 0;
+			    
+			    const selectablePegs = this.getSelectablePegs(pegLocations);
+			    
+			  	history.push({
+			  		selectedPeg:null,
+			  	  pegLocations: pegLocations,
+			  	  selectablePegs: selectablePegs,
+			  	  selectableHoles:null,
+			  	  lastMove: null   
+			  	});
+					
+					document.removeEventListener('click', this.unselectPeg, false);
+					
+					//did we win
+					if (selectablePegs.indexOf(1) === -1) { // there are no more valid moves
+						pegsRemaining = pegLocations.filter((itm) => {
+			    		return itm === 1;
+			    	}).length;
+			    	
+			    	let score = 0;
+			    	if (pegsRemaining === 1)
+			    		score = 5;
+			    	if (pegsRemaining === 2)
+			    		score = 3;
+			    	if (pegsRemaining === 3)
+			    		score = 1;	    			    	
+		    	
+		    		gameOver = 1;
+			    }				
+				}			
 			}
-		} else {
-			if (selectableHoles[i]) {
-		  	let lastMove = validMoves[selectedPeg].filter((move) => {
-		  	  return move[1] === i;
-		  	});
-		  	
-		    pegLocations[lastMove[0][0]] = 0;
-		    pegLocations[lastMove[0][1]] = 1;
-		    pegLocations[selectedPeg] = 0;
-		    
-		  	history.push({
-		  	  pegLocations: pegLocations,
-		  	  lastMove: lastMove   
-		  	});
-				
-				document.removeEventListener('click', this.unslectPeg, false);
-		  	
-		  	this.setState ({
-					selectedPeg: null,
-						history: history,					 
-				});
-			}
-		}		
+			
+	  	this.setState ({
+					history: history,		
+					gameOver: gameOver,
+					pegsRemaining: pegsRemaining			 
+			});		
+		}
 	} 
 	
 	render() {
+		const gameOver = this.state.gameOver;
 		const history = this.state.history;
 		const current = history[history.length - 1];
+		const selectedPeg = current.selectedPeg;
 		const pegLocations = current.pegLocations;
+		const selectablePegs = current.selectablePegs;
+		const selectableHoles = current.selectableHoles;
 		
-		let selectablePegs = 0;
-    let selectableHoles = 0;
-    
-    if(this.state.selectedPeg === null) {
-	    selectablePegs = pegLocations.map((hasPeg,idx) => {
-	     	if (hasPeg === 0) return 0;
-	     
-	     	const moves = this.props.validMoves[idx];
-	     	let retVal = 0;
-	     
-	     	for (let move of moves) {
-	      	if (pegLocations[move[0]] !== 0 && pegLocations[move[1]] !== 1)
-	        retVal = 1;     
-	     	}
-	     
-	     	return retVal;
-	    });	
-	    
-	    if (selectablePegs.indexOf(1) === -1) { // there are no more valid moves
-	    	if (this.state.gameOver !== 1) { // avoid infinite render loop
-	    	
-		    	let pegsRemaining = pegLocations.filter((itm) => {
-		    		return itm === 1;
-		    	}).length;
-		    	
-		    	let score = 0;
-		    	if (pegsRemaining === 1)
-		    		score = 5;
-		    	if (pegsRemaining === 2)
-		    		score = 3;
-		    	if (pegsRemaining === 3)
-		    		score = 1;	    			    	
-	    	
-	    		this.setState({
-		    			gameOver:1,
-		    			pegsRemaining: pegsRemaining,
-		    	});
-		    }
-	    }	
-	  } else {
-	  	selectableHoles = pegLocations.map((hasPeg,idx) => {
-	     if (hasPeg === 1) return 0;
-	     
-	     const moves = this.props.validMoves[this.state.selectedPeg];
-	     let retVal = 0;
-	     
-	     for (let move of moves) {
-	      if (move[1] === idx)
-	        retVal = 1;     
-	     }
-	     
-	     return retVal;
-	  	});
-	  }
+		console.log(history);
 		
 		return (
 				<Grid>
@@ -186,10 +214,10 @@ export default class CrackerBarrell extends React.Component {
 						<Container>	
 							<GameBoard 
 								pegLocations = {pegLocations}
-								selectedPeg = {this.state.selectedPeg}
+								selectedPeg = {selectedPeg}
 								selectablePegs = {selectablePegs}
 								selectableHoles = {selectableHoles}
-								onClick={(i) => this.handleClick(i, selectablePegs, selectableHoles)}/>
+								onClick={(i) => this.handleClick(i)}/>
 						</Container>								
 					</Grid.Row>
 				</Grid>
